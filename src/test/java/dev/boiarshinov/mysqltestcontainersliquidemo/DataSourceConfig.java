39539b480562c696210c1defa2c92d80d81acaf2
artem <artem.borya@gmail.com>
2021-03-14 22:03:07 +0300

    Add datasource config for tests

---
+++ src/test/java/dev/boiarshinov/mysqltestcontainersliquidemo/DataSourceConfig.java
@@ -0,0 +1,11 @@
+package dev.boiarshinov.mysqltestcontainersliquidemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@Configuration
+@ActiveProfiles("test")
+public class DataSourceConfig {
+
+
+}
