7203e7f81ebff1dd2c10218c6e1888b445643051
artem <artem.borya@gmail.com>
2021-03-14 22:42:47 +0300

    Add testing on testcontainers instead of real db

--- src/test/java/dev/boiarshinov/mysqltestcontainersliquidemo/DataSourceConfig.java
+++ src/test/java/dev/boiarshinov/mysqltestcontainersliquidemo/DataSourceConfig.java
@@ -1,11 +1,33 @@
 package dev.boiarshinov.mysqltestcontainersliquidemo;
 
+import javax.sql.DataSource;
+
+import com.mysql.cj.jdbc.Driver;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
 
 @Configuration
 @ActiveProfiles("test")
 public class DataSourceConfig {
 
+	private static final MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.23")
+			.withUsername( "user" )
+			.withPassword( "pass" );
 
+	static {
+		mysqlContainer.start();
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create()
+				.url( mysqlContainer.getJdbcUrl() )
+				.username( mysqlContainer.getUsername() )
+				.password( mysqlContainer.getPassword() )
+				.driverClassName( Driver.class.getName() )
+				.build();
+	}
 }
