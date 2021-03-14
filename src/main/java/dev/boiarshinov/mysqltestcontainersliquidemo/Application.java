75d85f6ca744c02de665305f249bdeb42957c0a4
artem <artem.borya@gmail.com>
2021-03-14 11:47:24 +0300

    Rename main class

--- src/main/java/dev/boiarshinov/mysqltestcontainersliquidemo/Application.java
+++ src/main/java/dev/boiarshinov/mysqltestcontainersliquidemo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysqlTestcontainersLiquiDemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run( MysqlTestcontainersLiquiDemoApplication.class, args );
+		SpringApplication.run( Application.class, args );
 	}
 
 }
